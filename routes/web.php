cba1201e79933197ec88157084abb3553c88c367
”33_mitsumasa_akiya” <kh1593ssss@gmail.com>
2020-09-04 05:43:45 +0000

    php09kadai

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 3【応用】 artisanを使って、Admin/ProfileControllerを作成しましょう。
+
+<?php
+Route:get('XXX', 'AAAController@bbb');
+?>
+
+<!--// 4【応用】 Admin/ProfileControllerに、以下のadd, create, edit, update それぞれのActionを追加してみましょう。-->
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
