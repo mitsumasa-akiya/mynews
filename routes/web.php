25916b3ca45d34cdf595bae43f98f81764cccb69
”33_mitsumasa_akiya” <kh1593ssss@gmail.com>
2020-09-20 01:48:57 +0000

    kadai_16

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('news', 'Admin\NewsController@index')->middleware('auth'); // 追記
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
+    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
 });
